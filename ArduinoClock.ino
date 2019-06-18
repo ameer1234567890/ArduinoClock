8c4e92e0834fe9df1285c7d576ab7380c736f4fc
Ameer Dawood <ameer1234567890@gmail.com>
2019-06-18 21:41:11 +0500

    OTA Progress

--- ArduinoClock.ino
+++ ArduinoClock.ino
@@ -91,6 +91,10 @@ void setup() {
 
   ArduinoOTA.setHostname(OTA_HOSTNAME);
   ArduinoOTA.begin();
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    display.set(String(progress / (total / 100)));
+    display.show();
+  });
 }
 
 
